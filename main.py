c6887e41fc625d7d045bc271d976e8233aa40828
Amandeep Rathee <arathee2@gmail.com>
2021-02-08 00:56:57 +0000

    add simple flask app

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
@@ -6,14 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     msg = """This is a simple flask app continuously being deployed using 
-    Google Cloud Platform. Append '/time' to the current URL to see today's
-    date and time.
+    Google Cloud Platform. Append '/time' after the base URL to the current 
+    URL to see today's date and time.
     """
     return msg
 
 @app.route('/time')
 def time():
-    return print(datetime.datetime.now())
+    msg = f"Current date and time is: {datetime.datetime.now()}"
+    return msg
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
