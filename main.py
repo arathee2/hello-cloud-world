ac04feea964aa0aab6bbfd774629bb67ec015bc4
Amandeep Rathee <arathee2@gmail.com>
2021-02-08 01:10:08 +0000

    add continuous delivery feature

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    msg = """This is a simple flask app continuously being deployed using 
+    msg = """This is a simple flask app that continuously deploys using 
     Google Cloud Platform. Append '/time' after the base URL to the current 
     URL to see today's date and time.
     """
